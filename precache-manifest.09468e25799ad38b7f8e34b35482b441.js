b52bbb0cc4735e20eec6d39c2609870520c09de4
Matthieu Brouillard <mbrouilla@gmail.com>
2019-01-31 14:59:17 +0100

    Updates

---
+++ precache-manifest.09468e25799ad38b7f8e34b35482b441.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "124647347acf255937e1",
+    "url": "/wemanity-challenge/static/js/runtime~main.12464734.js"
+  },
+  {
+    "revision": "590af451df225ffedd79",
+    "url": "/wemanity-challenge/static/js/main.590af451.chunk.js"
+  },
+  {
+    "revision": "e3959253b3698cecd589",
+    "url": "/wemanity-challenge/static/js/1.e3959253.chunk.js"
+  },
+  {
+    "revision": "590af451df225ffedd79",
+    "url": "/wemanity-challenge/static/css/main.f8281f01.chunk.css"
+  },
+  {
+    "revision": "1acf5b3a4000c81e499248dd813d48ce",
+    "url": "/wemanity-challenge/index.html"
+  }
+];
